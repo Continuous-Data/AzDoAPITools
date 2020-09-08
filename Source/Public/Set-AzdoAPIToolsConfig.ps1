93517d32c4441f1afee0d3ce3671dfb259835455
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2020-09-08 09:49:30 +0200

    single out-file command

--- Source/Public/Set-AzdoAPIToolsConfig.ps1
+++ Source/Public/Set-AzdoAPIToolsConfig.ps1
@@ -16,8 +16,7 @@ function Set-AzdoAPIToolsConfig {
             $existingconfig = Get-AzdoAPIToolsConfig -configfilepath $configfilepath
             
             if (confirm "Do you want to overwrite the existing config in [$configfilepath] (Y) or add to / replace in existing config (N)?") {
-                $config = Get-AzDoAPIToolsConfigDetails -new
-                $config | Out-File $configfilepath
+                $OutConfig = Get-AzDoAPIToolsConfigDetails -new
             }else{
                 $config = Get-AzDoAPIToolsConfigDetails | ConvertFrom-Json
                 
@@ -25,24 +24,29 @@ function Set-AzdoAPIToolsConfig {
                 
                 if( $matchingobject ){
                     Write-Host "Found [$($config.profilename)] in existing configfile. OverWriting existing entry"
+                    
                     $matchingobject.Organization = $config.Organization
                     $matchingobject.PAT = $config.PAT
 
-                    $existingconfig | ConvertTo-Json -Depth 3 | Out-File $configfilepath
+                    $OutConfig = $existingconfig | ConvertTo-Json -Depth 3
 
                 }else{
                     Write-Verbose "found no profile with current details. Adding the new config"
+                    
                     $existingconfig.profiles += $config
-                    $existingconfig | ConvertTo-Json -Depth 3 | Out-File $configfilepath
-                }
-                
+                    
+                    $OutConfig = $existingconfig | ConvertTo-Json -Depth 3
+                }     
             }
-            
-    
         }else{
             Write-verbose "no configfile found at $configfilepath. Continuing new file setup"
-            $config = Get-AzDoAPIToolsConfigDetails -new
-            $config | Out-File $configfilepath
+            $OutConfig = Get-AzDoAPIToolsConfigDetails -new
         }
+
+        if ($OutConfig) {
+            $OutConfig | Out-File $configfilepath
+        }
+        
+
     }
 }
