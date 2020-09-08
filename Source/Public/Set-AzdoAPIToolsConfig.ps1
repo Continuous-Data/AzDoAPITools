c80961f645e72ab7c2e0545b564f8cc09cad2654
Tobi Steenbakkers <tobi.steenbakkers@gmail.com>
2020-09-08 09:13:07 +0200

    adding possibility to overwrite existing profile

--- Source/Public/Set-AzdoAPIToolsConfig.ps1
+++ Source/Public/Set-AzdoAPIToolsConfig.ps1
@@ -15,13 +15,27 @@ function Set-AzdoAPIToolsConfig {
             Write-Verbose "Found an existing configfile in $configfilepath. loading it"
             $existingconfig = Get-AzdoAPIToolsConfig -configfilepath $configfilepath
             
-            if (confirm "Do you want to overwrite the existing config in [$configfilepath] (Y) or add to existing config (N)?") {
+            if (confirm "Do you want to overwrite the existing config in [$configfilepath] (Y) or add to / replace in existing config (N)?") {
                 $config = Get-AzDoAPIToolsConfigDetails -new
                 $config | Out-File $configfilepath
             }else{
-                $config = Get-AzDoAPIToolsConfigDetails
-                $existingconfig.profiles += $config | ConvertFrom-Json
-                $existingconfig | ConvertTo-Json -Depth 3 | Out-File $configfilepath
+                $config = Get-AzDoAPIToolsConfigDetails | ConvertFrom-Json
+                
+                $matchingobject = $existingconfig.profiles | Where-Object {$_.profilename -eq $config.profilename}
+                
+                if( $matchingobject ){
+                    Write-Host "Found [$($config.profilename)] in existing configfile. OverWriting existing entry"
+                    $matchingobject.Organization = $config.Organization
+                    $matchingobject.PAT = $config.PAT
+
+                    $existingconfig | ConvertTo-Json -Depth 3 | Out-File $configfilepath
+
+                }else{
+                    Write-Verbose "found no profile with current details. Adding the new config"
+                    $existingconfig.profiles += $config
+                    $existingconfig | ConvertTo-Json -Depth 3 | Out-File $configfilepath
+                }
+                
             }
             
     
